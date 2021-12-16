7d76d484400290d9f165578de9f6cd9184570b50
Seppi <seppi@ditached.de>
2021-12-16 10:43:35 +0100

    Inheritance

---
+++ Inheritance 1/Program.cs
@@ -0,0 +1,16 @@
+﻿new A().Print();
+new B().Print();
+new C().Print();
+
+public class A
+{
+    public void Print()
+    {
+        Console.WriteLine("Hallo");
+    }
+}
+
+public class B : A {}
+public class C : A {}
+
+
